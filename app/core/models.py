947a62bb568a21f4f3a00d07d29bf9c938ead99a
JahazielHernandezHoyos <jahazielhernandezhoyoz@gmail.com>
2021-12-23 13:07:06 -0500

    main

--- app/core/models.py
+++ app/core/models.py
@@ -2,8 +2,18 @@ from django.db import models
 from datetime import *
 
 # Create your models here.
+class type(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name="Tipo"
+        verbose_name_plural = "Tipo"
+        ordering = ['id']
 
 class Employee(models.Model):
+    type = models.ForeignKey(type, on_delete=models.CASCADE)
     names = models.CharField(max_length=100, verbose_name='Nombres')
     ci = models.CharField(max_length=10, unique=True, verbose_name='Cedula de Identidad')
     date_joined = models.DateTimeField(default=datetime.now, verbose_name='Fecha de registro')
@@ -12,6 +22,7 @@ class Employee(models.Model):
     age = models.PositiveIntegerField(default=0,verbose_name='Edad')
     salary = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name='Salario')
     state = models.BooleanField(default=True)
+    #gender = models.CharField(max_length=50)
     avatar = models.ImageField(max_length=100, upload_to='avatars/', null=True, blank=True)
     cvitae = models.FileField(upload_to='cvitae/', null=True, blank=True)
 
